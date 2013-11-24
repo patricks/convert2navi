de0624e5ba2fef55ffc2381b6eb5d0b5167f6310
Patrick <patrick@helmsdeep.at>
2013-11-24 12:59:38 +0100

    fixed some typos

--- convert.sh
+++ convert.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-$INPUT=at_gc_tradi.ov2
-$OUTPUT=at_gc_tradi.csv
+INPUT=at_gc_tradi.ov2
+OUTPUT=at_gc_tradi.csv
 
-gpsbabel -i tomtom -f $INPUT -o csv,style=mystyle.style -F $OUTPUT
+gpsbabel -i tomtom -f $INPUT -o csv,style=navigon.style -F $OUTPUT
